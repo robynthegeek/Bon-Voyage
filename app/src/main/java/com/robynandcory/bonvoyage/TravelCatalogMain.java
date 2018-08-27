e2311b87112f9ebdacce3b1459f18e974f9c2380
Robyn Klopp <rklopp@gmail.com>
2018-08-27 13:54:32 -0700

    clean build for Udacity Submission

--- app/src/main/java/com/robynandcory/bonvoyage/TravelCatalogMain.java
+++ app/src/main/java/com/robynandcory/bonvoyage/TravelCatalogMain.java
@@ -19,8 +19,13 @@ import com.robynandcory.bonvoyage.data.TravelDbHelper;
 
 /**
  * Project 8 for Udacity ABND
+ *
+ * Current app contains a single SQLite table to store inventory information for a travel store.
+ * UI to be completed in phase 2 of the project.
+ *
  * Icons paid use from https://gumroad.com/d/302e27c9605ad25705945f65e006e1a4
  */
+
 public class TravelCatalogMain extends AppCompatActivity {
 
     private TravelDbHelper mDbHelper;
@@ -45,15 +50,18 @@ public class TravelCatalogMain extends AppCompatActivity {
         testWriteTravelDB();
         //reads from DB and displays in textView on the screen.  For debugging and grading only.
         testReadTravelDB();
-
     }
 
+    /**
+     * Inserts 3 rows test data to the Travel Database
+     * For grading and debugging purposes only.
+     */
     private void testWriteTravelDB() {
         mDbHelper = new TravelDbHelper(this);
         SQLiteDatabase wDB = mDbHelper.getWritableDatabase();
 
         // Clear DB by deleting all entries before adding new test data
-        wDB.delete(TravelContract.TravelEntry.TABLE_NAME,null,null);
+        wDB.delete(TravelContract.TravelEntry.TABLE_NAME, null, null);
 
         // Adds 3 rows of test data
         ContentValues contentValues = new ContentValues();
@@ -86,10 +94,12 @@ public class TravelCatalogMain extends AppCompatActivity {
         contentValues.put(TravelContract.TravelEntry.COLUMN_SUPPLIER_PHONE, "503.555.1384");
 
         wDB.insert(TravelContract.TravelEntry.TABLE_NAME, null, contentValues);
+    }
 
-
-        }
-
+    /**
+     * Reads the rows of test data and displays the results on the screen.
+     * For grading and debugging purposes only.
+     */
     private void testReadTravelDB() {
         SQLiteDatabase db = mDbHelper.getReadableDatabase();
         String[] projection = {
@@ -102,6 +112,7 @@ public class TravelCatalogMain extends AppCompatActivity {
                 TravelContract.TravelEntry.COLUMN_SUPPLIER,
                 TravelContract.TravelEntry.COLUMN_SUPPLIER_PHONE,
         };
+
         // Query selects all results from current inventory of test data
         Cursor cursor = db.query(
                 TravelContract.TravelEntry.TABLE_NAME,
@@ -119,7 +130,7 @@ public class TravelCatalogMain extends AppCompatActivity {
 
         try {
             rowCountView.setText("There are currently " + cursor.getCount() + " items in your inventory.");
-            testQueryView.setText("Inventory Contains: ");
+            testQueryView.setText("Inventory Contains: \n");
             testQueryView.append(TravelContract.TravelEntry._ID + " - " +
                     TravelContract.TravelEntry.COLUMN_NAME + " - " +
                     TravelContract.TravelEntry.COLUMN_PRICE + " - " +
@@ -148,33 +159,21 @@ public class TravelCatalogMain extends AppCompatActivity {
                 String currentSupplier = cursor.getString(supplierColumnIndex);
                 String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
                 testQueryView.append(("\n" + currentID + " - " +
-                currentName + " - " +
-                currentPrice + " - " +
-                currentQuantity + " - " +
-                currentCategory + " - " +
-                currentSeason + " - " +
-                currentSupplier + " - " +
-                currentSupplierPhone));
+                        currentName + " - " +
+                        currentPrice + " - " +
+                        currentQuantity + " - " +
+                        currentCategory + " - " +
+                        currentSeason + " - " +
+                        currentSupplier + " - " +
+                        currentSupplierPhone));
             }
 
         } finally {
             cursor.close();
         }
-//                "SELECT * FROM " + TravelContract.TravelEntry.TABLE_NAME, null);
-//        try {
-//            TextView testQueryView = (TextView) findViewById(R.id.test_query);
-//            testQueryView.setText("There are currently " + cursor.getCount() + " items in your inventory.");
-//        } finally {
-//            cursor.close();
-//        }
-//                TravelContract.TravelEntry.TABLE_NAME,
-//                projection,
-//                null,
-//                null,
-//                null,
-//                null,
-//                null);
     }
+
+//ToDo Add support for menu options in the UI phase.
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
