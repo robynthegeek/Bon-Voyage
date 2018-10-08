72e9d772a2a9f298a46593d743191a8e6c7af760
Robyn Klopp <rklopp@gmail.com>
2018-10-08 12:58:49 -0700

    Add sale button functionality

--- app/src/main/java/com/robynandcory/bonvoyage/TravelCursorAdapter.java
+++ app/src/main/java/com/robynandcory/bonvoyage/TravelCursorAdapter.java
@@ -1,8 +1,10 @@
 package com.robynandcory.bonvoyage;
 
 import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -14,8 +16,10 @@ import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
 import static com.robynandcory.bonvoyage.data.TravelContract.TravelEntry;
 
@@ -40,17 +44,17 @@ public class TravelCursorAdapter extends RecyclerView.Adapter<TravelCursorAdapte
              * Locates view and uses cursor to bind data to views for given context
              */
             @Override
-            public void bindView(View view, Context context, Cursor cursor) {
+            public void bindView(View view, final Context context, Cursor cursor) {
                 TextView nameTextView = view.findViewById(R.id.list_item_name);
                 TextView quantityTextView = view.findViewById(R.id.list_item_quantity);
                 TextView priceTextView = view.findViewById(R.id.list_item_price_amount);
 
                 int nameColumnIndex = cursor.getColumnIndex(TravelEntry.COLUMN_NAME);
-                int quantityColumnIndex = cursor.getColumnIndex(TravelEntry.COLUMN_QUANTITY);
+                final int quantityColumnIndex = cursor.getColumnIndex(TravelEntry.COLUMN_QUANTITY);
                 int priceColumnIndex = cursor.getColumnIndex(TravelEntry.COLUMN_PRICE);
 
-                String itemName = cursor.getString(nameColumnIndex);
-                String itemQuantity = cursor.getString(quantityColumnIndex);
+                final String itemName = cursor.getString(nameColumnIndex);
+                final String itemQuantity = cursor.getString(quantityColumnIndex);
                 int itemPrice = cursor.getInt(priceColumnIndex);
 
                 nameTextView.setText(itemName);
@@ -60,11 +64,30 @@ public class TravelCursorAdapter extends RecyclerView.Adapter<TravelCursorAdapte
 
                 int cursorItemId = cursor.getInt(cursor.getColumnIndex(TravelEntry._ID));
                 final Uri contentUri = Uri.withAppendedPath(TravelEntry.CONTENT_URI, Integer.toString(cursorItemId));
+
+                Button saleButton = view.findViewById(R.id.sale_button);
+                saleButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int quantityInt = Integer.parseInt(itemQuantity);
+                        if (quantityInt > 0) {
+                            quantityInt -= 1;
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put(TravelEntry.COLUMN_QUANTITY, quantityInt);
+                            context.getContentResolver().update(contentUri, contentValues, null, null);
+                        } else {
+                            Toast.makeText(context, "Cannot set negative stock amount", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
             }
         };
 
     }
-    private String formatPrice (int intPrice) {
+
+    private String formatPrice(int intPrice) {
         return Integer.toString(intPrice);
     }
 
@@ -89,7 +112,7 @@ public class TravelCursorAdapter extends RecyclerView.Adapter<TravelCursorAdapte
 
     /**
      * @param viewGroup parent
-     * @param position the view type
+     * @param position  the view type
      * @return a new viewHolder to inflate the layout
      */
     @Override
